8328e0786c0723ae8613e6dec0e4ce5532579e51
BanfiFederico <10581441@polimi.it>
2021-11-22 10:39:59 +0100

    test commit db

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "testdynamodb": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
